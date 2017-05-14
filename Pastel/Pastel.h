51bfd8680a0e382da4756d28140a56f227b1e5b5
cruz <cruzdiary@gmail.com>
2017-05-14 17:57:29 +0900

    Create shared framework schemes

---
+++ Pastel/Pastel.h
@@ -0,0 +1,19 @@
+//
+//  Pastel.h
+//  Pastel
+//
+//  Created by CruzDiary on 14/05/2017.
+//  Copyright © 2017 Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pastel.
+FOUNDATION_EXPORT double PastelVersionNumber;
+
+//! Project version string for Pastel.
+FOUNDATION_EXPORT const unsigned char PastelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pastel/PublicHeader.h>
+
+
