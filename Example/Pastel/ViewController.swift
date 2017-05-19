43f53172663665d799a21b83b87c89ab2e5be437
Seungyoun Yi <younatics@gmail.com>
2017-05-19 21:33:23 +0900

    bug fixed in Example

--- Example/Pastel/ViewController.swift
+++ Example/Pastel/ViewController.swift
@@ -23,15 +23,15 @@ class ViewController: UIViewController {
         let pastelView = PastelView(frame: view.bounds)
         
         // Custom Direction
-        pastelView.startPastelPoint = .bottomLeft
-        pastelView.endPastelPoint = .topRight
+        pastelView.startPoint = .bottomLeft
+        pastelView.endPoint = .topRight
         
         // Custom Duration
         pastelView.animationDuration = 3.0
         
         // Custom Color
         
-        pastelView.setColors([UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha: 1.0),
+        pastelView.setColors(colors: [UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha: 1.0),
                               UIColor(red: 255/255, green: 64/255, blue: 129/255, alpha: 1.0),
                               UIColor(red: 123/255, green: 31/255, blue: 162/255, alpha: 1.0),
                               UIColor(red: 32/255, green: 76/255, blue: 255/255, alpha: 1.0),
