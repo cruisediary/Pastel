e3070765f9ed460fa4e96c651dab1c2d615a9d3b
Michał Czerniakowski <mpczerniakowski@gmail.com>
2018-02-21 10:58:12 +0100

    Remove unneeded code, run swiftlint autocorrect.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -19,7 +19,7 @@ class TableOfContentsSpec: QuickSpec {
             it("will eventually fail") {
                 expect("time").toEventually( equal("done") )
             }
-            
+
             context("these will pass") {
 
                 it("can do maths") {
